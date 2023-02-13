6201f202c11516bcec1a538b5724cc979c9eafb7
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-13 15:54:16 +0530

    add docstrings for the dataloader for the skeleton items

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,6 +3,9 @@ from torch_geometric.data import Dataset, Data
 
 class MoleculeDataset(Dataset):
     def __init__(self):
+        """
+        Constructor for the data loader
+        """
         pass
     
     def __len__(self) -> int:
@@ -13,36 +16,45 @@ class MoleculeDataset(Dataset):
     
     @property
     def num_nodes(self) -> int:
+        """Number of nodes/molecules in the dataset."""
         pass
     
     @property
     def feature_names(self):
+        """Names of the node features."""
         pass
 
     @property
     def has_labels(self):
+        """Check if the dataset has labels."""
         pass
 
     @property
     def data_source_type(self):
+        """The type of data source used for this dataset."""
         pass
 
     @classmethod
     def from_moleculenet(cls):
+        """Factory method to create MoleculeDataset from MoleculeNet dataset."""
         pass
 
     @classmethod
     def from_smiles_file(cls):
+        """Factory method to create MoleculeDataset from a file containing SMILES strings."""
         pass
 
     @classmethod
     def from_compound_name_file(cls):
+        """Factory method to create MoleculeDataset from a file containing compound names."""
         pass
 
     @classmethod
     def from_smiles(cls):
+        """Factory method to create MoleculeDataset directly from SMILES string(s)."""
         pass
 
     @classmethod
     def from_compound_names(cls):
+        """Factory method to create MoleculeDataset directly from compound name(s)."""
         pass
