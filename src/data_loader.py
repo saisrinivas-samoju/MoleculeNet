95dfe2187d669661436bf0d2b1302c695656578a
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-13 16:57:34 +0530

    add constructor code with mappers

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,11 +2,26 @@ from typing import *
 from torch_geometric.data import Dataset, Data
 
 class MoleculeDataset(Dataset):
-    def __init__(self):
+    def __init__(self, root: str = None, name: Literal["ESOL", "FreeSolv", "Lipo", "PCBA", "MUV", "HIV", "BACE", "BBBP", "Tox21", "ToxCast", "SIDER", "ClinTox"] = None, filepath: str = None, smiles_colname: str = 'smiles', label_colname: str = 'label', compound_name_colname: str = 'compound_name', filetype: Literal['smiles', 'compound_name'] = None, smiles: Union[str, List[str]] = None, compound_names: Union[str, List[str]] = None):
         """
         Constructor for the data loader
         """
-        pass
+        self.root = root
+        self.name = name
+        self.filepath = filepath
+        self.smiles_colname = smiles_colname
+        self.label_colname = label_colname
+        self.compound_name_colname = compound_name_colname
+        self.smiles = smiles
+        self.compound_names = compound_names
+        if filetype == 'compound_name':
+            self.is_compound_name_file = True
+            self.is_smiles_file = False
+        else:
+            self.is_compound_name_file = False
+            self.is_smiles_file = True
+
+        self.__load_data()
     
     def __len__(self) -> int:
         pass
