faab24503e1e8afab16035a581a6e513273c6050
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-15 12:22:39 +0530

    add docstrings for the code skeleton of data explorer

--- src/data_explorer.py
+++ src/data_explorer.py
@@ -7,11 +7,39 @@ from src.data_loader import MoleculeDataset
 
 
 def make_df_for_eda(dataset: MoleculeDataset) -> pd.DataFrame:
+    """Make a dataframe for EDA from a MoleculeDataset.
+
+    Args:
+        dataset (MoleculeDataset): MoleculeDataset
+
+    Returns:
+        pd.DataFrame: A dataframe for EDA
+    """
     pass
 
 
 def plot_histograms(data_list: list[dict]) -> go.Figure:
+    """
+    Create plotly histograms from a list of dictionaries.
+    
+    Parameters:
+    -----------
+    data_list : list of data dict
+    
+    Returns:
+    --------
+    fig : plotly.graph_objects.Figure
+        Plotly figure with the histograms
+    """
     pass
 
 def explore_with_histograms(dataset: MoleculeDataset) -> go.Figure:
+    """Explore a MoleculeDataset with histograms.
+
+    Args:
+        dataset (MoleculeDataset): MoleculeDataset
+
+    Returns:
+        go.Figure: A plotly figure with the histograms
+    """
     pass
