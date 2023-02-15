4341a003ff4ecb9a4514fe5939226a907ca5d6e9
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-15 11:35:24 +0530

    add code skeleton for data explorer

---
+++ src/data_explorer.py
@@ -0,0 +1,17 @@
+from typing import *
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from src.data_loader import MoleculeDataset
+
+
+def make_df_for_eda(dataset: MoleculeDataset) -> pd.DataFrame:
+    pass
+
+
+def plot_histograms(data_list: list[dict]) -> go.Figure:
+    pass
+
+def explore_with_histograms(dataset: MoleculeDataset) -> go.Figure:
+    pass
