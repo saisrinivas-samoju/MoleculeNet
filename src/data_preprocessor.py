30e11c3481fc1b2e57bab3b331427f0aec0a9683
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-18 01:44:33 +0530

    add get_balance_data method to data preprocessor

--- src/data_preprocessor.py
+++ src/data_preprocessor.py
@@ -31,7 +31,37 @@ class Preprocessor:
         return filtered_dataset
     
     def get_balance_data(self):
-        pass
+        label_list = [data.y for data in self.dataset]
+        unique, counts = np.unique(label_list, return_counts=True)
+        counts_dict = dict(zip(unique, counts))
+        label_prop_dict = {label: counts_dict[label] / len(label_list) for label in unique}
+        if self.balance_strategy == 'oversampling':
+            idx_values_to_add = []
+            for label in unique:
+                if label_prop_dict[label] < self.balance_threshold:
+                    diff_prop = self.balance_threshold - label_prop_dict[label]
+                    num_samples_to_add = int(diff_prop * len(label_list))
+                    class_data_idx_list = [idx for idx, data in enumerate(self.dataset) if data.y == label]
+                    if num_samples_to_add > len(class_data_idx_list):
+                        replace = True
+                    else:
+                        replace = False
+                    random_idx_values = np.random.choice(range(len(class_data_idx_list)), size=num_samples_to_add, replace=replace)
+                    idx_values_to_add.extend(random_idx_values)
+            res_dataset = [self.dataset[idx] for idx in idx_values_to_add] + self.dataset.data_list
+        elif self.balance_strategy == 'undersampling':
+            idx_value_to_remove = []
+            for label in unique:
+                if label_prop_dict[label] > self.balance_threshold:
+                    diff_prop = label_prop_dict[label] - self.balance_threshold
+                    num_samples_to_remove = int(diff_prop * len(label_list))
+                    class_data_idx_list = [idx for idx, data in enumerate(self.dataset) if data.y == label]
+                    random_idx_values = np.random.choice(range(len(class_data_idx_list)), size=num_samples_to_remove, replace=False)
+                    idx_value_to_remove.extend(random_idx_values)
+            res_dataset = [self.dataset[idx] for idx in idx_value_to_remove]
+        else:
+            raise ValueError(f"Invalid balance strategy: {self.balance_strategy} | balance_strategy must be either 'oversampling' or 'undersampling'")
+        return res_dataset
     
     def preprocess(self): # main item
         pass
