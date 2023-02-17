9fb20ef378d4bf3473b16d1ff0d40b83f172b28d
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-18 01:12:10 +0530

    add data preprocessor constructor

--- src/data_preprocessor.py
+++ src/data_preprocessor.py
@@ -1,11 +1,24 @@
+from typing import *
 from src.data_loader import MoleculeDataset
 
 class Preprocessor:
     """Preprocessing steps in the class:
     - Removing outliers based on the labels if the task is regression.
     - Oversampling/undersampling based on a threshold value if the task is classification."""
-    def __init__(self):
-        pass
+    def __init__(self, dataset: MoleculeDataset, task: Literal["regression", "classification"], balance_data: bool = False, balance_threshold: float = 0.2, balance_strategy: Literal["oversampling", "undersampling"] = "oversampling", remove_outliers: bool = False, outlier_threshold: float = 3.5):
+        self.dataset = dataset
+        self.task = task
+        if self.task == "classification":
+            self.balance_data = balance_data
+            self.num_classes = self.dataset.num_classes
+            balance_threshold = balance_threshold if balance_threshold < 0.5 else (1 - balance_threshold)
+            # considering balanced threshold value is based on binary class division though there are multiple classes
+            balance_threshold = balance_threshold * 2 / self.num_classes
+            self.balance_threshold = balance_threshold
+            self.balance_strategy = balance_strategy
+        elif self.task=="regression":
+            self.remove_outliers = remove_outliers
+            self.outlier_threshold = outlier_threshold
     
     def remove_outliers(self):
         pass
