41f2a5dab2e607cfea2419783380bbeed484289c
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-19 17:05:30 +0530

    add ShuffleSplit strategy code

--- src/data_splitter.py
+++ src/data_splitter.py
@@ -1,5 +1,7 @@
 # Let's develop the data spliting based on strategy design
 from abc import ABC, abstractmethod
+import random
+import torch
 from torch_geometric.loader import DataLoader
 
 # Base Strategy Class
@@ -30,5 +32,37 @@ class SimpleSplit(DataSplitStrategy):
         train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
         val_loader = DataLoader(val_dataset, batch_size=batch_size)
         test_loader = DataLoader(test_dataset, batch_size=batch_size)
+        
+        return train_loader, val_loader, test_loader
+    
+class ShuffleSplit(DataSplitStrategy):
+    def split(self, dataset, train_ratio=0.7, val_ratio=0.15, test_ratio=0.15, batch_size=32, random_seed=42):
+        train_ratio, val_ratio, test_ratio = self.standardize_ratios(train_ratio, val_ratio, test_ratio)
+        # Set random seed for reproducibility
+        torch.manual_seed(random_seed)
+        random.seed(random_seed)
+        
+        # Create a list of indices and shuffle them
+        indices = list(range(len(dataset)))
+        random.shuffle(indices)
+        
+        # Calculate split sizes
+        train_size = int(train_ratio * len(dataset))
+        val_size = int(val_ratio * len(dataset))
+        
+        # Split indices
+        train_indices = indices[:train_size]
+        val_indices = indices[train_size:train_size + val_size]
+        test_indices = indices[train_size + val_size:]
+        
+        # Create dataset subsets
+        train_dataset = [dataset[i] for i in train_indices]
+        val_dataset = [dataset[i] for i in val_indices]
+        test_dataset = [dataset[i] for i in test_indices]
+        
+        # Create data loaders
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, drop_last=True)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, drop_last=True)
         
         return train_loader, val_loader, test_loader
