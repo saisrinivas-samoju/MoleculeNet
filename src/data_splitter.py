6fc9e251c694b08eeca5141db187edbb20fba6d4
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-19 15:44:56 +0530

    add base strategy skeleton for data splitter

--- src/data_splitter.py
+++ src/data_splitter.py
@@ -1,1 +1,11 @@
 # Let's develop the data spliting based on strategy design
+from abc import ABC, abstractmethod
+
+# Base Strategy Class
+class DataSplitStrategy(ABC):
+    def standardize_ratios(self):
+        pass
+    
+    @abstractmethod
+    def split(self):
+        pass
