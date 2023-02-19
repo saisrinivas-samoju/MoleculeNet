253597e1971a293d4b2336fed4701b377f226f75
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-19 16:15:39 +0530

    add DataSplitterStrategy class

--- src/data_splitter.py
+++ src/data_splitter.py
@@ -3,9 +3,18 @@ from abc import ABC, abstractmethod
 
 # Base Strategy Class
 class DataSplitStrategy(ABC):
-    def standardize_ratios(self):
-        pass
+    def standardize_ratios(self, train_ratio=0.7, val_ratio=0.15, test_ratio=0.15):
+        train_ratio = abs(train_ratio)
+        val_ratio = abs(val_ratio)
+        test_ratio = abs(test_ratio)
+        if train_ratio + val_ratio + test_ratio != 1:
+            total_ratio = train_ratio + val_ratio + test_ratio
+            train_ratio = train_ratio / total_ratio
+            val_ratio = val_ratio / total_ratio
+            test_ratio = test_ratio / total_ratio
+        return train_ratio, val_ratio, test_ratio
     
     @abstractmethod
-    def split(self):
+    def split(self, dataset, train_ratio=0.7, val_ratio=0.15, test_ratio=0.15, batch_size=32):
+        """Split a dataset into train, validation and test sets"""
         pass
