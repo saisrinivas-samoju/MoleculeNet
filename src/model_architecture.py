cbc4b741afdbd0cc68741b479d667c76b1860c20
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-26 11:26:33 +0530

    add moleculenetclassifier full constructor code.

--- src/model_architecture.py
+++ src/model_architecture.py
@@ -37,8 +37,35 @@ class MoleculeNetRegressor(Module):
         pass
     
 class MoleculeNetClassifier(Module):
-    def __init__(self):
-        pass
+    def __init__(self, num_features, hidden_dim=64, layer_type='gcn', dropout_rate=0.2, num_classes=2):
+        super(MoleculeNetClassifier, self).__init__()
+        torch.manual_seed(42)
+        
+        # Layer type selection
+        if layer_type == 'gcn':
+            self.conv1 = GCNConv(num_features, hidden_dim)
+            self.conv2 = GCNConv(hidden_dim, hidden_dim)
+            self.conv3 = GCNConv(hidden_dim, hidden_dim)
+        elif layer_type == 'gat':
+            self.conv1 = GATConv(num_features, hidden_dim)
+            self.conv2 = GATConv(hidden_dim, hidden_dim)
+            self.conv3 = GATConv(hidden_dim, hidden_dim)
+        elif layer_type == 'sage':
+            self.conv1 = SAGEConv(num_features, hidden_dim)
+            self.conv2 = SAGEConv(hidden_dim, hidden_dim)
+            self.conv3 = SAGEConv(hidden_dim, hidden_dim)
+        
+        # Batch normalization
+        self.bn1 = BatchNorm1d(hidden_dim)
+        self.bn2 = BatchNorm1d(hidden_dim)
+        self.bn3 = BatchNorm1d(hidden_dim)
+        
+        # Dropout
+        self.dropout = Dropout(dropout_rate)
+        
+        # Output layer
+        self.out = Linear(hidden_dim * 2, num_classes)
+        self.num_classes = num_classes
     
     def forward(self):
         pass
