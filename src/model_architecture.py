7b0f8f928b1fa60fa9374a577d3cc6fbbd30f9d5
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-26 11:24:50 +0530

    add model_architecture skeleton for both classification and regression models

---
+++ src/model_architecture.py
@@ -0,0 +1,15 @@
+from torch.nn import Module
+
+class MoleculeNetRegressor(Module):
+    def __init__(self):
+        pass
+    
+    def forward(self):
+        pass
+    
+class MoleculeNetClassifier(Module):
+    def __init__(self):
+        pass
+    
+    def forward(self):
+        pass
