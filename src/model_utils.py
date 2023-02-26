71ca3aab65bc58e67dab417d7bd38a9c05942047
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-02-26 16:11:34 +0530

    add typehints and docstrings for model utils

--- src/model_utils.py
+++ src/model_utils.py
@@ -1,9 +1,47 @@
 import os
+from typing import *
 import torch
 from src.model_architecture import MoleculeNetRegressor, MoleculeNetClassifier
 
-def save_model():
+def save_model(model, optimizer, model_info, metrics, model_path, model_name, task_type: Literal['classification', 'regression']) -> None:
+    """
+    Save model with metadata
+    
+    Parameters:
+    -----------
+    model : torch.nn.Module
+        The model to save
+    optimizer : torch.optim.Optimizer
+        The optimizer used for training
+    model_info : dict
+        Dictionary containing model hyperparameters
+    metrics : dict
+        Dictionary containing evaluation metrics
+    model_path : str
+        Directory to save model
+    model_name : str
+        Name of the model file
+    """
     pass
 
-def load_model():
+def load_model(model_path, device, task_type: Literal['classification', 'regression']) -> tuple:
+    """
+    Load model with metadata
+    
+    Parameters:
+    -----------
+    model_path : str
+        Path to the saved model file
+    device : torch.device
+        Device to load the model on
+        
+    Returns:
+    --------
+    model : torch.nn.Module
+        The loaded model
+    model_info : dict
+        Dictionary containing model hyperparameters
+    metrics : dict
+        Dictionary containing evaluation metrics
+    """
     pass
