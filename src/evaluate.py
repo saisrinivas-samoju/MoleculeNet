7be546f9a291e2c90300af41d04cd49cbfbd9f04
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-03-05 08:36:29 +0530

    add code skeleton for evaluate.py

---
+++ src/evaluate.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import *
+import torch
+
+def evaluate_model():
+    pass
+
+# only for classification items
+def plot_confusion_matrix():
+    pass
+
+# only for classification items
+def evaluate_and_visualize():
+    pass
