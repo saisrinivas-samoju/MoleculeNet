19dc0cc5cd6eabdd35c79951ac8647d43fd8731d
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-03-11 13:21:30 +0530

    add train_epoch function code extending to classification models

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import time
+from typing import Literal
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +9,7 @@ import torch.nn as nn
 import torch.optim as optim
 from src.evaluate import evaluate_model
 
-def train_epoch(model, optimizer, train_loader, criterion, device):
+def train_epoch(model, optimizer, train_loader, criterion, device, task_type:Literal['classification', 'regression']):
     model.train()
     total_loss = 0
     num_batches = 0
@@ -27,10 +28,31 @@ def train_epoch(model, optimizer, train_loader, criterion, device):
         
         # Forward pass
         output = model(x, edge_index, batch)
-        target = data.y.view(-1, 1).float()
-        
-        # Calculate loss
-        loss = criterion(output, target)
+        if task_type == 'regression':
+            target = data.y.view(-1, 1).float()
+            
+            # Calculate loss
+            loss = criterion(output, target)
+        elif task_type == 'classification':
+            target = data.y.long()
+            # Calculate loss
+            if model.num_classes == 2:
+                target = target.float()
+                # Make sure dimensions match by using the minimum size
+                min_size = min(output.size(0), target.size(0))
+                output_selected = output[:min_size, 0]  # Take first min_size elements, column 0
+                target_selected = target[:min_size]      # Take first min_size elements
+                
+                loss = criterion(output_selected, target_selected)
+            else:
+                # For multi-class, also ensure sizes match
+                min_size = min(output.size(0), target.size(0))
+                output_selected = output[:min_size]
+                target_selected = target[:min_size]
+                
+                loss = criterion(output_selected, target_selected)
+        else:
+            raise Exception(f"InvalidTaskType: {task_type}")
         
         # Backward pass
         loss.backward()
