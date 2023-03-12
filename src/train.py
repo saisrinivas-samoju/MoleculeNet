9ad7b47b7b8b745b78096695294b11f9b5293001
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-03-12 09:59:44 +0530

    add train_epoch function code for both regression and classification tasks using private helper functions

--- src/train.py
+++ src/train.py
@@ -8,8 +8,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from src.evaluate import evaluate_model
+from config import task_type
 
-def train_epoch(model, optimizer, train_loader, criterion, device, task_type:Literal['classification', 'regression']):
+def train_epoch(model, optimizer, train_loader, criterion, device, task_type:Literal['classification', 'regression']=task_type):
     model.train()
     total_loss = 0
     num_batches = 0
@@ -67,7 +68,7 @@ def train_epoch(model, optimizer, train_loader, criterion, device, task_type:Lit
     epoch_loss = total_loss / max(num_batches, 1)
     return epoch_loss
 
-def train_model(model, optimizer, train_loader, val_loader, device, num_epochs=100, 
+def _train_reg_model(model, optimizer, train_loader, val_loader, device, num_epochs=100, 
                 patience=10, criterion=None, scheduler=None, verbose=True):
     # Set up loss function if not provided
     if criterion is None:
@@ -151,3 +152,97 @@ def train_model(model, optimizer, train_loader, val_loader, device, num_epochs=1
         model.load_state_dict(best_model_state)
     
     return model, history, best_metrics
+
+def _train_clf_model(model, optimizer, train_loader, val_loader, device, num_epochs=100, 
+                patience=10, criterion=None, scheduler=None, verbose=True):
+    # Set up loss function if not provided
+    if criterion is None:
+        if model.num_classes == 2:
+            criterion = nn.BCELoss()
+        else:
+            criterion = nn.CrossEntropyLoss()
+    
+    # Initialize tracking variables
+    best_val_f1 = 0.0
+    best_model_state = None
+    best_metrics = None
+    counter = 0
+    
+    # History tracking
+    history = {
+        'train_loss': [],
+        'val_loss': [],
+        'val_metrics': [],
+        'epoch_times': []
+    }
+    
+    # Main training loop
+    for epoch in range(num_epochs):
+        start_time = time.time()
+        
+        # Train for one epoch
+        train_loss = train_epoch(model, optimizer, train_loader, criterion, device)
+        
+        # Evaluate on validation set
+        val_metrics, _, _ = evaluate_model(model, val_loader, device)
+        val_loss = val_metrics['Loss']
+        
+        # Update learning rate if scheduler is provided
+        if scheduler is not None:
+            prev_lr = optimizer.param_groups[0]['lr']
+            
+            scheduler.step(1.0 - val_metrics['F1_macro'])
+            
+            if hasattr(scheduler, 'get_last_lr'):
+                current_lr = scheduler.get_last_lr()[0]
+            else:
+                current_lr = optimizer.param_groups[0]['lr']
+            
+            if prev_lr != current_lr and verbose:
+                print(f"Learning rate changed from {prev_lr:.6f} to {current_lr:.6f}")
+        
+        # Record history
+        epoch_time = time.time() - start_time
+        history['train_loss'].append(train_loss)
+        history['val_loss'].append(val_loss)
+        history['val_metrics'].append(val_metrics)
+        history['epoch_times'].append(epoch_time)
+        
+        # Print progress
+        if verbose:
+            print(f"Epoch {epoch+1}/{num_epochs} | "
+                  f"Train Loss: {train_loss:.6f} | "
+                  f"Val Loss: {val_loss:.6f} | "
+                  f"Val F1 (macro): {val_metrics['F1_macro']:.6f} | "
+                  f"Val Accuracy: {val_metrics['Accuracy']:.6f} | "
+                  f"Time: {epoch_time:.2f}s")
+        
+        # Check if this is the best model so far
+        if val_metrics['F1_macro'] > best_val_f1:
+            best_val_f1 = val_metrics['F1_macro']
+            best_model_state = model.state_dict().copy()
+            best_metrics = val_metrics.copy()
+            counter = 0
+        else:
+            counter += 1
+            
+        # Early stopping
+        if counter >= patience:
+            if verbose:
+                print(f"Early stopping at epoch {epoch+1}")
+            break
+    
+    # Load the best model
+    if best_model_state is not None:
+        model.load_state_dict(best_model_state)
+    
+    return model, history, best_metrics
+
+def train_model(model, optimizer, train_loader, val_loader, device, num_epochs=100, 
+                patience=10, criterion=None, scheduler=None, verbose=True, task_type:Literal['classification','regression']=task_type):
+    if task_type=='classification':
+        return _train_clf_model(model=model, optimizer=optimizer, train_loader=train_loader, val_loader=val_loader, device=device, num_epochs=num_epochs, patience=patience, criterion=criterion, scheduler=scheduler, verbose=verbose)
+    elif task_type=='regression':
+        return _train_reg_model(model=model, optimizer=optimizer, train_loader=train_loader, val_loader=val_loader, device=device, num_epochs=num_epochs, patience=patience, criterion=criterion, scheduler=scheduler, verbose=verbose)
+    else:
+        raise ValueError(f"InvalidTaskType: {task_type}")
