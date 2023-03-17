fe3e0f57ef873c9b8eea3abbaf3a62238467e222
Sai Srinivas <saisrinivas.samoju@gmail.com>
2023-03-17 21:08:43 +0530

    add predict_molecule code for regression in predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,31 @@
-def predict_molecule():
-    pass
+import torch
+from torch_geometric.utils import from_smiles
+
+def predict_molecule(model, data, device):
+    model.eval()
+    with torch.no_grad():
+        # Convert SMILES to data object if a string is provided
+        if isinstance(data, str):
+            try:
+                data = from_smiles(data)
+            except Exception as e:
+                print(f"Error converting SMILES to graph: {e}")
+                return None
+        
+        # Create a batch with just this molecule
+        data = data.to(device)
+        
+        # Our model expects batched data, so we need to add batch information
+        if not hasattr(data, 'batch') or data.batch is None:
+            data.batch = torch.zeros(data.x.size(0), dtype=torch.long, device=device)
+        
+        # Ensure correct data types
+        x = data.x.float()  # Convert features to float
+        edge_index = data.edge_index.long()  # Convert edge indices to long
+        batch = data.batch.long()  # Convert batch indices to long
+        
+        prediction = model(x, edge_index, batch)  # Use type-converted tensors
+        return prediction.item()
 
 def predict_molecules():
     pass
